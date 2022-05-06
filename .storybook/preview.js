1b5814469d529ec6317ac76e57c29b9b187550d7
hafizglasswall <96419297+hafizglasswall@users.noreply.github.com>
2022-05-06 18:57:26 +0100

    added parameters in preview js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,3 +8,14 @@ Object.defineProperty(NextImage, "default", {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
+
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
